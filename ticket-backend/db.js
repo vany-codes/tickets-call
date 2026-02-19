dbea4e11711daa129ba7d98dd80163d0585f5e6c
Jhovan Hernandez Ramirez <perjhoal@gmail.com>
2026-02-18 22:41:27 -0600

    Creando backend para tickets callcenter

---
+++ ticket-backend/db.js
@@ -0,0 +1,10 @@
+const { Pool } = require('pg'); // Importa el módulo 'pg' para manejar la conexión a PostgreSQL
+require('dotenv').config(); // Carga las variables de entorno desde el archivo .env
+
+// Configura la conexión a la base de datos utilizando las variables de entorno
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
+
+module.exports = pool; // Exporta el pool de conexiones para ser utilizado en otros archivos
